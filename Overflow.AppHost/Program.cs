290ed6278c9e29ba34ac21ed0673f9d94726a95e
mark coates <miscm2@btinternet.com>
2025-11-23 22:11:52 +0000

    End of Section 3

--- Overflow.AppHost/Program.cs
+++ Overflow.AppHost/Program.cs
@@ -3,4 +3,16 @@ var builder = DistributedApplication.CreateBuilder(args);
 var keyClock = builder.AddKeycloak("keycloak", 6001)
     .WithDataVolume("keycloak-data");
 
+var postgres = builder.AddPostgres("postgres", port: 5432)
+    .WithDataVolume("postgres-data")
+    .WithPgAdmin();
+
+var questionDb = postgres.AddDatabase("questionDb");
+
+var questionService = builder.AddProject<Projects.QuestionService>("question-svc")
+    .WithReference(keyClock)
+    .WithReference(questionDb)
+    .WaitFor(keyClock)
+    .WaitFor(questionDb);
+
 builder.Build().Run();
